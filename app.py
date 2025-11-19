992943c3d40294775e7a9540f3fa2d2c34a397f3
angxlCM <angelcruz161205@gmail.com>
2025-11-19 09:16:57 -0500

    cambios en puertos

--- app.py
+++ app.py
@@ -29,10 +29,7 @@ def enviar_ultima():
 def home():
     return "API Flask activa y funcionando."
 
-# Solo para desarrollo local, Railway ignora esto
-if __name__ == '__main__':
-    app.run(debug=True)
-
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
